baa9db01da86eea3766232e41b59d29912352f2d
ddiu8081 <ddiu8081@gmail.com>
2022-02-23 21:02:52 +0800

    docs: add credit item

--- src/components/Credits.svelte
+++ src/components/Credits.svelte
@@ -3,15 +3,22 @@
   <ul class="mt-2 opacity-50">
     <li class="mt-1">
       Font is 
-      <a href="https://fonts.google.com/specimen/Exo+2" class="text-teal-600">Exo 2</a>
+      <a href="https://fonts.google.com/specimen/Exo+2" target="_blank" class="text-teal-600">Exo 2</a>
        under 
-      <a href="https://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=OFL" class="text-teal-600">SIL License</a>.
+      <a href="https://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=OFL" target="_blank" class="text-teal-600">SIL License</a>
+      .
     </li>
     <li class="mt-1">
       Image generated with
-      <a href="https://www.python.org/" class="text-teal-600">Python</a>
+      <a href="https://www.python.org/" target="_blank" class="text-teal-600">Python</a>
        and 
-      <a href="https://pillow.readthedocs.io/" class="text-teal-600">Pillow</a>.
+      <a href="https://pillow.readthedocs.io/" target="_blank" class="text-teal-600">Pillow</a>
+      .
+    </li>
+    <li class="mt-1">
+      Backend on 
+      <a href="https://cloud.tencent.com/product/scf" target="_blank" class="text-teal-600">Tencent Serverless Cloud Function</a>
+      .
     </li>
   </ul>
 </div>
